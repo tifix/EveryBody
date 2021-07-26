ddd3c827ff27498ac71746717b3b2cab49baf815
Mikołaj czarnecki <wertynius3@gmail.com>
2021-07-26 01:49:28 +0100

    MenuAndIntro
    for not dropping you straight into the action.
    Neon signs paid extra.

---
+++ Assets/Scripts/SceneSwitcher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public static SceneSwitcher instance;
+    public bool is_debugging = false;
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null) { instance = this; DontDestroyOnLoad(this); }
+        else Destroy(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //utilities
+        // if (is_debugging && Input.GetKeyDown(KeyCode.Space)) new Note().play(); //SpawnAnticipationNote(anticip_note, new Transform[] { A_pos, S_pos, K_pos, L_pos }[Random.Range(0, 4)],new Note(4));
+        if (is_debugging && Input.GetKeyDown(KeyCode.LeftShift)) Time.timeScale = 0.25f;
+        if (is_debugging && Input.GetKeyUp(KeyCode.LeftShift)) Time.timeScale = 1;
+        if (is_debugging && Input.GetKeyDown(KeyCode.LeftAlt)) anim.SetTrigger("TriggerCrossfade");
+    }
+
+    public static void LoadScene(int scene_number) 
+    {
+        SceneManager.LoadScene(scene_number);
+    } 
+}
